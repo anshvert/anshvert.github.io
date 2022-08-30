c6205357ac0296436cfb3df01e0cce17438cfca3
Ansh Tyagi <anshtyagi2222@gmail.com>
2022-08-31 02:03:58 +0530

    Added Main Page Theme

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,17 @@
-function open_projects(){
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-    console.log('hello')
-
-}
+//// Get the offset position of the navbar
+//var sticky = navbar.offsetTop;
+//
+//// When the user scrolls the page, execute myFunction
+//window.onscroll = function() {myFunction()};
+//
+//// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+//function myFunction() {
+//  if (window.pageYOffset >= sticky) {
+//    navbar.classList.add("sticky")
+//  } else {
+//    navbar.classList.remove("sticky");
+//  }
+//}
